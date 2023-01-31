a73cf158211c96d2df899b02838aeb18253a0caf
Andrey Volynkin <kazakmt4@gmail.com>
2023-01-31 11:41:30 +0300

    пункты ДЗ 1-8

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    protected $users;
+
+    protected function getPath(Request $request)
+    {
+        return $request->path();
+    }
+
+    protected function getUrl(Request $request)
+    {
+        return $request->url();
+    }
+
+    public function index()
+    {
+        return view('form');
+    }
+
+    public function store(Request $request)
+    {
+        $users = [
+            'firstName' => $request->input('firstName'),
+            'lastName' => $request->input('lastName'),
+            'email' => $request->input('email'),
+            'position' => $request->input('position'),
+            'address' => $request->input('address')
+        ];
+
+        var_dump($users);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $users = [
+            'id' => $id,
+            'firstName' => $request->json()->get('firstName'),
+            'lastName' => $request->json()->get('lastName'),
+            'email' => $request->json()->get('email'),
+            'position' => $request->json()->get('position'),
+            'address' => $request->json()->get('address')
+        ];
+
+        var_dump($users);
+    }
+}
+
