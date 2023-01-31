b14db084609dc4162b119381c41a1a7e6a8a61dc
Andrey Volynkin <kazakmt4@gmail.com>
2023-01-31 09:21:52 +0300

    test comit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>5.7 Практическая работа</title>
+</head>
+<body>
+<div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+    <h1>
+        5.7 Практическая работа
+    </h1>
+</div>
+
+<div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+    </div>
+</div>
+</body>
+</html>
